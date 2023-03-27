b3e0819f2bce73be30a341c721ecd19f1a339590
eduard2276 <31226875+eduard2276@users.noreply.github.com>
2023-03-27 13:20:36 +0300

    Update Publish.js
    Test

--- app/(Screens)/(app)/Publish.js
+++ app/(Screens)/(app)/Publish.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Text } from 'react-native-paper'
-import { default as Menu } from '../../components/common/Footer'
+import React from "react";
+import { Text } from "react-native-paper";
+import { default as Menu } from "../../components/common/Footer";
 
 const Publish = () => {
   return (
     <>
-        <Text>Publish</Text>
+      <Text>Publish2</Text>
     </>
-  )
-}
+  );
+};
 
 export default Publish;
