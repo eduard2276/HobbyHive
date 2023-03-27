efc14c5316fac432fcfe1a97e84c0c7ffc332a92
eduard2276 <31226875+eduard2276@users.noreply.github.com>
2023-03-27 16:31:05 +0300

    Publish

--- app/(Screens)/(app)/Publish.js
+++ app/(Screens)/(app)/Publish.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { Text } from "react-native-paper";
+import { Text, SafeAreaView, StyleSheet } from "react-native";
 import { default as Menu } from "../../components/common/Footer";
+import { sports } from "../../constants/";
+import { SelectList } from "react-native-dropdown-select-list";
 
 const Publish = () => {
+  const [selected, setSelected] = React.useState("");
   return (
-    <>
-      <Text>Publish2</Text>
-    </>
+    <SafeAreaView style={styles.container}>
+      <SelectList
+        setSelected={(val) => setSelected(val)}
+        data={sports}
+        save="value"
+        // arrowicon={
+        //   <FontAwesome name="chevron-down" size={12} color={"black"} />
+        // }
+        //searchicon={<FontAwesome name="search" size={12} color={"black"} />}
+        // boxStyles={{ borderRadius: 20 }} //override default styles
+      />
+    </SafeAreaView>
   );
 };
 
 export default Publish;
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: "center",
+    alignItems: "center",
+    width: "90%",
+    backgroundColor: "#FFFFFF",
+    flex: 1,
+  },
+});
---
+++ app/constants/sports.js
@@ -0,0 +1,10 @@
+const sports = [
+  { key: "1", value: "Fotball" },
+  { key: "2", value: "Baschet" },
+  { key: "3", value: "Tennis" },
+  { key: "4", value: "Table Tennis" },
+  { key: "5", value: "Gym" },
+  { key: "6", value: "Bicycle Ride" },
+];
+
+export default sports;
